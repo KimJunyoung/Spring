14d93f50f8ca7e49781f306403f6138f17646d25
Kimjunyoung <985151@naver.com>
2023-07-20 22:53:24 +0900

    Spring_Inflean_RoadMap_1_Finished

---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+    private String nam;
+
+    public String getName() {
+        return nam;
+    }
+
+    public void setName(String name) {
+        this.nam = name;
+    }
+}
